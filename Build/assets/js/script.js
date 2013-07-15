b94ddb390bc20cfd57449c0c806d3ba693fe4ffe
Rodrigo Passos <rodrigopassos@mac.com>
2013-07-15 11:25:22 -0400

    experiments with zoom

--- Build/assets/js/script.js
+++ Build/assets/js/script.js
@@ -1,53 +1,20 @@
 (function() {
 
   $(function() {
-    var carsousel, carsouselClickAction, carsouselOptions, carsouselReadyAction, zoomImageWindowResizeAction;
+    var options;
     $(document).foundation();
-    zoomImageWindowResizeAction = function() {
-      var demo4obj, winWidth;
-      demo4obj = $('#demo4').data('imagezoom');
-      winWidth = $(window).width();
-      if (winWidth > 900) {
-        return demo4obj.changeZoomSize(480, 300);
-      } else {
-        return demo4obj.changeZoomSize(winWidth * 0.4, winWidth * 0.4 * 0.625);
-      }
+    options = {
+      zoomType: 'standard',
+      lens: true,
+      preloadImages: true,
+      alwaysOn: false,
+      zoomWidth: 300,
+      zoomHeight: 300,
+      xOffset: 0,
+      yOffset: 0,
+      position: 'left'
     };
-    carsouselClickAction = function(el, pos, evt) {
-      var demo4obj;
-      el.siblings().removeClass("active");
-      el.addClass("active");
-      carsousel.setCurrent(pos);
-      evt.preventDefault();
-      demo4obj = $('#demo4').data('imagezoom');
-      return demo4obj.changeImage(el.find('img').attr('src'), el.find('img').data('largeimg'));
-    };
-    carsouselReadyAction = function() {
-      var imageZoomOptions;
-      imageZoomOptions = {
-        type: 'standard',
-        zoomSize: [480, 300],
-        bigImageSrc: 'demo_assets/large/2.jpg',
-        offset: [10, -4],
-        zoomViewerClass: 'standardViewer',
-        onShow: function(obj) {
-          return obj.$viewer.hide().fadeIn(500);
-        },
-        onHide: function(obj) {
-          return obj.$viewer.show().fadeOut(500);
-        }
-      };
-      $('#product-image-spotlight').ImageZoom(imageZoomOptions);
-      $('#product-image-carousel li:eq(0)').addClass('active');
-      return $(window).resize(zoomImageWindowResizeAction);
-    };
-    carsouselOptions = {
-      start: 1,
-      minItems: 4,
-      onClick: carsouselClickAction,
-      onReady: carsouselReadyAction
-    };
-    return carsousel = $('#product-image-carousel').elastislide(carsouselOptions);
+    return $('.product-detail-image').jqzoom(options);
   });
 
 }).call(this);
--- assets/js/script.coffee
+++ assets/js/script.coffee
@@ -2,50 +2,15 @@ $ ->
 	$(document).foundation()
 	# Write scripts here
 
-	zoomImageWindowResizeAction = ()->
-		demo4obj = $('#demo4').data('imagezoom')
-		winWidth = $(window).width()
-		if winWidth > 900
-			demo4obj.changeZoomSize(480,300)
-		else
-			demo4obj.changeZoomSize( winWidth*0.4,winWidth*0.4*0.625)
-		
+	options = 
+		zoomType: 'standard'
+		lens:true
+		preloadImages: true
+		alwaysOn:false
+		zoomWidth: 300
+		zoomHeight: 300
+		xOffset:0
+		yOffset:0
+		position:'left'
 
-	carsouselClickAction = ( el, pos, evt )->
-		el.siblings().removeClass "active"
-		el.addClass "active"
-		carsousel.setCurrent pos
-		evt.preventDefault()
-		# for imagezoom to change image 
-		demo4obj = $('#demo4').data('imagezoom')
-		demo4obj.changeImage(el.find('img').attr('src'),el.find('img').data('largeimg'))
-
-	carsouselReadyAction = ()->
-		# init imagezoom with many options
-		imageZoomOptions = {
-			type: 'standard'
-			zoomSize: [480,300]
-			bigImageSrc:'demo_assets/large/2.jpg'
-			offset:[10,-4]
-			zoomViewerClass:'standardViewer'
-			onShow: (obj)->
-				obj.$viewer.hide().fadeIn(500)
-			onHide: (obj)->
-				obj.$viewer.show().fadeOut(500)
-		}
-
-		$('#product-image-spotlight').ImageZoom imageZoomOptions
-		
-		$('#product-image-carousel li:eq(0)').addClass('active')
-		
-		# change zoomview size when window resize
-		$(window).resize zoomImageWindowResizeAction
-
-	carsouselOptions = {
-		start: 1
-		minItems: 4
-		onClick: carsouselClickAction
-		onReady: carsouselReadyAction
-	}
-
-	carsousel = $('#product-image-carousel').elastislide carsouselOptions
+	$('.product-detail-image').jqzoom( options );  
